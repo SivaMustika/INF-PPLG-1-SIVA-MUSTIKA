86a211fa27d27d671ae65f301aad3b911b26cde8
SivaMustika <evosluminaire41@gmail.com>
2025-05-08 15:35:36 +0700

    tugas variable siva

---
+++ var.py
@@ -0,0 +1,10 @@
+# Contoh variabel dengan input dari pengguna
+
+# Mengambil input dari pengguna
+nama = input("siva mustika: ")
+umur = input("16 tahun: ")
+hobi = input("mengaji ")
+
+# Menampilkan kembali data
+print("Hai,", nama)
+print("Kamu berumur", umur, "tahun dan hobi kamu adalah", hobi)
