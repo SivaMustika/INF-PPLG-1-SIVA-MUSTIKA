8f30e244b281c6f2ec8359b4bc2dbc18d29d6243
SivaMustika <evosluminaire41@gmail.com>
2025-05-08 15:34:30 +0700

    tuags pengulangan

---
+++ pengulangan.py
@@ -0,0 +1,18 @@
+# WHILE LOOP
+print("Contoh WHILE LOOP:")
+i = 1
+while i <= 5:
+    print("Siva")
+    i += 1
+
+# FOR LOOP
+print("\nContoh FOR LOOP:")
+for i in range(5):
+    print("Siva")
+
+# NESTED LOOP
+print("\nContoh NESTED LOOP:")
+for i in range(3):  # Baris
+    for j in range(3):  # Kolom
+        print("Siva", end=" ")
+    print()  # pindah ke baris baru
